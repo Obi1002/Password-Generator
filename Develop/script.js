4a274b2f2089a13974f29732e51f08512a1483f8
Obi <obiude1002@gmail.com>
2021-02-18 01:54:27 -0500

    added the characters

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,13 @@
 // Assignment Code
+// Added all of the special characters for the password to generate
 var generateBtn = document.querySelector("#generate");
+var lowercaseList = "abcdefghijklmnopqrstuvwxyz";
+var uppercaseList = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numberList = "0123456789";
+var specialList = "~`!@#$%^&*()+=[]"
+
+
+
 
 // Write password to the #password input
 function writePassword() {
